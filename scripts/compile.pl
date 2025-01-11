aa510cfa95bf86bb2a2c74dc60b3aaa7686efd2f
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-01-11 15:55:47 +0000

    Use proper fxtran-acdc

--- scripts/compile.pl
+++ scripts/compile.pl
@@ -3,9 +3,7 @@
 use strict;
 use local::lib;
 use FindBin qw ($Bin);
-#use lib "$Bin/../../../../lib";
-#use lib '/home/gmap/mrpm/marguina/fxtran-acdc/lib';
-use lib '/home/gmap/mrpm/marguina/cucalln/fxtran-acdc/lib';
+use lib '/home/gmap/mrpm/marguina/fxtran-acdc/lib';
 use FileHandle;
 use File::Copy;
 use File::Basename;
