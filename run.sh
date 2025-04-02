eab7326cb86d783c1b6273c0de4c6696304a4cb5
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-04-02 15:22:15 +0000

    Fix cuctracer.F90 + add fxtran-acdc

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ export OMP_NUM_THREADS=8
 
 cd $SLURM_SUBMIT_DIR
 
-NGPBLKS=50
+NGPBLKS=500
 
 # --case-out . \
 # --case-in . \
@@ -22,13 +22,13 @@ NGPBLKS=50
 ./compile.gpu_nvhpc_d_ref/main_cucalln_mf.x \
   --ngpblks $NGPBLKS \
   --case-in /scratch/work/marguina/cucalln2_d \
-  --verbose --diff --method openaccsinglecolumn > stdeo.ref.txt 2>&1
+  --verbose --stat --method openaccsinglecolumn > stdeo.ref.txt 2>&1
 
 
 ./compile.gpu_nvhpc_d/main_cucalln_mf.x \
   --ngpblks $NGPBLKS \
   --heapsize 1000 \
   --case-in /scratch/work/marguina/cucalln2_d \
-  --verbose --diff --method openaccsinglecolumn > stdeo.new.txt 2>&1
+  --verbose --stat --method openaccsinglecolumn > stdeo.new.txt 2>&1
 
  vim -d stdeo.ref.txt stdeo.new.txt
