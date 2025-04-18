d2ea981c29dc59d70cf20fd8543ec65245105f64
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-04-18 12:11:09 +0000

    Change number of blocks

--- run.sh
+++ run.sh
@@ -14,23 +14,25 @@ export OMP_NUM_THREADS=8
 
 cd $SLURM_SUBMIT_DIR
 
+TIMES=10
+
 ./compile.cpu_intel_s/main_cucalln_mf.x \
-  --ngpblks 6000 \
+  --ngpblks 6000 --times $TIMES --out stat.txt \
   --case-in /scratch/work/marguina/cucalln2_s \
   --verbose --stat --method openmp
 
 ./compile.gpu_nvhpc_s/main_cucalln_mf.x \
-  --ngpblks 6000 \
+  --ngpblks 6000 --times $TIMES --out stat.txt \
   --case-in /scratch/work/marguina/cucalln2_s \
   --verbose --stat --method openaccsinglecolumn
 
 ./compile.cpu_intel_d/main_cucalln_mf.x \
-  --ngpblks 6000 \
+  --ngpblks 6000 --times $TIMES --out stat.txt \
   --case-in /scratch/work/marguina/cucalln2_d \
   --verbose --stat --method openmp
 
 ./compile.gpu_nvhpc_d/main_cucalln_mf.x \
-  --ngpblks 6000 \
+  --ngpblks 6000 --times $TIMES --out stat.txt \
   --case-in /scratch/work/marguina/cucalln2_d \
   --verbose --stat --method openaccsinglecolumn
 
