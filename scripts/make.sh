631ff5aa01a1b8206f3170478a331b5d4cc3bef7
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-05-22 11:36:32 +0000

    Add test case cucalln2_d_10 (only 10 blocks)

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,13 +2,18 @@
 
 set -x
 
+arch=$1
+
+if [ "x$arch" = "x" ]
+then
 arch=gpu_nvhpc_d
+fi
 
 \rm -rf compile.$arch
 mkdir compile.$arch
 cd compile.$arch
 
-Makefile.PL --include '$(SRC)/../Makefile.gpu_nvhpc_d' --SRC ../src
+Makefile.PL --include "\$(SRC)/../Makefile.$arch" --SRC ../src
 
 make -j8
 
