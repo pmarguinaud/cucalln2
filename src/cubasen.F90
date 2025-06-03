d1795881b908b5188bffe11bafa89dfa0cb5e15a
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-06-03 19:36:30 +0000

    Compile & run with singleblock

--- src/cubasen.F90
+++ src/cubasen.F90
@@ -8,7 +8,7 @@ SUBROUTINE CUBASEN &
  & KLAB,     LDCUM,    LDSC,     KCBOT,    KBOTSC,&
  & KCTOP,    KDPL,     PCAPE )  
 
-!$ACDC pointerparallel --ydcpg_opts
+!$ACDC singleblock
 
 !          THIS ROUTINE CALCULATES CLOUD BASE FIELDS
 !          CLOUD BASE HEIGHT AND CLOUD TOP HEIGHT
--- src/cumastrn.F90
+++ src/cumastrn.F90
@@ -19,7 +19,7 @@ SUBROUTINE CUMASTRN &
  & PMFUDE_RATE,        PMFDDE_RATE,    PCAPE,   PWU, PWMEAN, PVDISCU, PDISS, &
  & KTRAC,    PCEN,     PTENC,    PSCAV, PSCAV0 )  
 
-!$ACDC pointerparallel --ydcpg_opts
+!$ACDC singleblock
 
 !**** *CUMASTR*  MASTER ROUTINE FOR CUMULUS MASSFLUX-SCHEME
 
