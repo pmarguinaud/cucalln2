228707a0e8dcda0472864486d03a9d062493c2a2
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2025-06-05 14:28:02 +0000

    Transform the code with the manyblocks method

--- src/cubasen.F90
+++ src/cubasen.F90
@@ -8,7 +8,7 @@ SUBROUTINE CUBASEN &
  & KLAB,     LDCUM,    LDSC,     KCBOT,    KBOTSC,&
  & KCTOP,    KDPL,     PCAPE )  
 
-!$ACDC singleblock
+!$ACDC manyblocks
 
 !          THIS ROUTINE CALCULATES CLOUD BASE FIELDS
 !          CLOUD BASE HEIGHT AND CLOUD TOP HEIGHT
